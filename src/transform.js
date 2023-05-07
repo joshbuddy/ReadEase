e0414404ad8432e31887e7fe07e8bb5afb833512
Joshua Hull <josh@fireflop.com>
2023-05-08 00:19:09 +0200

    Add naive gibberish detection

--- src/transform.js
+++ src/transform.js
@@ -1,10 +1,16 @@
 import normalize from "./normalize";
+import gibberish from 'gibberish-detective'
+
+const detector = gibberish({useCache: false})
 
 function transform(text) {
   const parts = text.split(/(?=[\s.,\/;:!?]+)|(?<=[\s.,\/;:!?])/g)
   for (const i in parts) {
     const normalPart = normalize("NFKD", parts[i]);
     if (normalPart === parts[i]) {
+      continue
+    }
+    if (detector.detect(normalPart)) {
       continue
     }
     parts[i] = normalPart
